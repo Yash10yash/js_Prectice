910489937f6fafd717c8f7f8b13e734517fb047d
Yash gupta <yg745192@gmail.com>
2026-02-27 10:19:14 +0530

    First commit

---
+++ 01-Script.js/script.js
@@ -0,0 +1,6 @@
+const accountNumber = 123456789;
+let accountEmail = "example@example.com";
+var accountPassword = "147258";
+accountCity = "New Delhi";
+
+console.table({ accountNumber, accountEmail, accountPassword, accountCity });
---
+++ script.js
@@ -0,0 +1,5 @@
+var a = 1;
+a= 2; // This line would cause an error because 'a' is a constant
+// let c = a + b;
+console.log(a);
+//console.log("Hello, World!");
