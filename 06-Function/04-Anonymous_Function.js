d2f91623b783993d6c451ba444509156c4024f20
Yash gupta <yg745192@gmail.com>
2026-03-05 23:38:04 +0530

    some more changes

---
+++ 06-Function/04-Anonymous_Function.js
@@ -0,0 +1,13 @@
+
+// Anonymous Function : A function without a name is called an anonymous function.
+// Anonymous functions are often used as arguments to other functions, 
+// such as in callbacks or event handlers.
+
+// Example of an anonymous function used as a callback in setTimeout
+
+let x = function() {
+    console.log("This is an anonymous function.");
+}
+
+x(); // This will call the anonymous function and print the message to the console.
+
---
+++ 06-Function/05-Expression_Function.js
@@ -0,0 +1,14 @@
+// Expression function 
+// Expression function is a function that is assigned to a variable. It can be anonymous or named.
+
+// Example of an anonymous function assigned to a variable
+let greet = function() {
+    console.log("Hello, World!");
+}
+greet(); // This will call the expression function and print the message to the console.
+
+// Example of a named function assigned to a variable
+let add = function sum(a, b) {
+    return a + b;
+}
+add(5, 3); // This will call the expression function and return the sum of 5 and 3.
