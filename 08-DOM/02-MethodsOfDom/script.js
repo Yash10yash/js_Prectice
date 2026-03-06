4f0024b9bc8bb6428ade4f91793f0a8e9fe673eb
Yash gupta <yg745192@gmail.com>
2026-03-07 00:45:14 +0530

    some more

--- 08-DOM/02-MethodsOfDom/script.js
+++ 08-DOM/02-MethodsOfDom/script.js
@@ -32,4 +32,9 @@ let f = document.querySelector(".tech"); // get the first element with class "te
 console.log(f.innerText); // Show inner text of the first element with class "tech";
 
 let g = document.querySelector("h2"); // get the first h2 element;
-console.log(g.innerText); // Show inner text of the first h2 element;   
+
+
+let var2 = document.querySelector(".classs");
+var2.classList = "hello helo hola"; // Change the class of the first element with class "classs" to "hello helo hola";
+var2.setAttribute("class", "hello helo hola"); // Change the class of the first element with class "classs" to "hello helo hola";
+console.log(var2); // Show the updated class of the first element with class "classs";
