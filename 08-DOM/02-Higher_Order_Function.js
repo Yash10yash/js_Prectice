782fdcd42ba9ffb5bf563d26f2afbaf3ebc14836
Yash gupta <yg745192@gmail.com>
2026-03-08 00:48:15 +0530

    changes

---
+++ 08-DOM/02-Higher_Order_Function.js
@@ -0,0 +1,23 @@
+const higherOrder = n => {
+    const oneFun = m => {
+        const twoFun = p => {
+            return n + m + p;
+        }
+        return twoFun;
+    }
+    return oneFun;
+}
+
+console.log(higherOrder(5)(10)(15));
+
+let arr = [1, 2, 3, 4, 5];
+
+const sumArray = arr => {
+    let total =0;
+    arr.forEach(element => {
+        total += element;
+    });
+    return total;
+}
+
+console.log(sumArray(arr));
