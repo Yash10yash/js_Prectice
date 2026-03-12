be59bc87858dc981dc6629c55768bdd690316c40
Yash gupta <yg745192@gmail.com>
2026-03-12 23:34:10 +0530

    a

--- 01-DataType/04-prectice.js
+++ 01-DataType/04-prectice.js
@@ -1,13 +1,13 @@
-// var x = 100;
-// {
-//     var x = 200;
-// }
+var x = 100;
+{
+    var x = 200;
+}
 
-// var a = x;
-//  {
-//     var a = 300;
-//  }
-//  console.log(a);
+var a = x;
+ {
+    var a = 300;
+ }
+ console.log(a);
 console.log([] == ![]);
 let arr = [1,2,3,4,4];
 arr.map((item,index)=>{
